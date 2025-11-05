22b1743869499322862ae6d1877e8e8f5ba80d1d
notreallymani <manikantaboddu5@gmail.com>
2025-11-05 11:54:18 +0530

    Fix OAuth configuration and add authentication routes

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
+require('./services/passport');
 
-app.get('/', (req, res) => {
-  res.send({ hi: 'there' });
-});
+mongoose.connect(require('./config/keys').mongoURI);
+
+const app = express();
+require('./routes/authRoutes')(app);
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
-
